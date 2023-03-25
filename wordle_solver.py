689ae6da13b92d01452156dbec635f6d9809bc84
corinnt <94402869+corinnt@users.noreply.github.com>
2023-03-25 00:31:49 -0400

    improved error handling in parse_input

--- wordle_solver.py
+++ wordle_solver.py
@@ -15,11 +15,12 @@ def main(index_to_words_dicts, answers_set):
     populate_index_to_words_dicts(index_to_words_dicts, answers_set)
     user_input = input("guess details> ").lower().split()
     while user_input != "found":
-        locked_dict, loose_dict, continue_flag = read_locked_loose(user_input)
-        if continue_flag:
+        locked_dict, loose_dict, continue_guessing_flag = parse_input(user_input)
+        if continue_guessing_flag:
             guess_set = find_guesses(locked_dict, loose_dict, index_to_words_dicts, answers_set)
             print("possible guesses: " + str(guess_set))
         user_input = input("guess details > ").lower().split()
+
 
 def populate_index_to_words_dicts(index_to_word_list_dicts, answers_set):
     with open('valid_words.csv') as csv_file:
@@ -35,54 +36,65 @@ def populate_index_to_words_dicts(index_to_word_list_dicts, answers_set):
                         index_to_word_list_dicts[index][letter] = {word} # add letter to dict for first time
                     index+=1
 
-def read_locked_loose(user_input):
+def parse_input(user_input):
     locked_dict = {} 
     loose_dict = {}
     remaining_indices = range(WORD_LEN)
-    #control flags
-    lock_loose = -1 #initialize as -1 to be neither locked mode nor loose mode
-    prev_is_alpha = False 
-    continue_flag = True
+    exit = ({}, {}, False)
     for i in range(len(user_input)):
         arg = user_input[i] 
         if arg == "found":
             print("Congrats!")
-            continue_flag = False
+            return exit
         # begin locked / green args
         elif arg == "-g":
-            lock_loose = 0
+            status = "locked"
         # begin loose / yellow args
-        elif arg == "-y": 
-            lock_loose = 1
-
-        elif len(arg) > 1 or lock_loose < 0:
-            print("incorrect argument")
-            continue_flag = False
-        # parsing locked args
-        elif lock_loose == 0 and arg.isalpha() and not prev_is_alpha: 
-            try:
-                arg_as_int = int(user_input[i + 1])
-                prev_is_alpha = True
-            except: 
-                print("index was not an integer")
-                continue_flag = False
-            if arg in locked_dict.items():
-                locked_dict[arg] = locked_dict[arg].append(arg_as_int)
-            else:
-                locked_dict[arg] = [arg_as_int]
-        # lock_loose = 1 -> loose letters to follow
-        elif lock_loose == 0 and prev_is_alpha:  
-            try:
-                remaining_indices.remove(int(arg))
-                prev_is_alpha = False
-            except:   # 'index' couldn't be removed, was out of range or not a number
-                print("'index' was out of range, duplicate, or not a number @" + str(i) + ", " + arg)
-                continue_flag = False
-
+        elif arg == "-y":
+            status = "loose"
+        # check that a flag was passed
+        if status == "locked" or status == "loose":
+            dict_to_fill = eval(status + "_dict")
+            i = parse_guess_details(dict_to_fill, user_input, i, remaining_indices, status)
+            if (i < 0): 
+                return exit
         else:
-            loose_dict[arg] = remaining_indices # loose dict: maps letter to list of poss indices
+            print("Incorrect usage. Correct usage: -g <letter> <position> -y <letter> <position> <position>")
+            return exit 
+    return locked_dict, loose_dict, True
 
-    return locked_dict, loose_dict, continue_flag
+def parse_guess_details(to_fill_dict, user_input, prev_index, loose_indices, status):
+    locked_letter = "0"
+    exit = -1
+    for i in range(prev_index + 1, len(user_input)):
+        arg = user_input[i]
+        if arg.isalpha() and locked_letter not in to_fill_dict.keys():
+            print("Missing indices for a " + status + " letter '" + locked_letter + "'")
+            return exit
+        elif arg.isalpha():
+            locked_letter = arg 
+        elif not arg.isalpha() and locked_letter != "0":
+            try:
+                index_of_letter = int(user_input[i]) - 1
+            except: 
+                print("Index was not an integer or improperly entered.")
+                return exit
+            if index_of_letter < 0:
+                print("Input position of letter must be between 1 and " + str(WORD_LEN) + ".")
+                return exit
+            if locked_letter not in to_fill_dict.keys(): # todo: way to speed this up?
+                to_fill_dict[locked_letter] = [index_of_letter]
+            else:
+                to_fill_dict[locked_letter] = to_fill_dict[locked_letter].append(index_of_letter)
+            loose_indices.remove(index_of_letter)
+        elif arg == "-y" or arg == "-g":
+            return i - 1
+        else:
+            print("Unknown error in parsing locked arguments.")
+            return exit
+    print("dbg: returning i = " + str(i) + " from parse_locked")
+    return i # possbug: need to return len(user_input) - 1 ?
+
 
 def find_guesses(locked_dict, loose_dict, index_to_words_dicts, answers_set):
     # populate suggested set of locked letters
@@ -132,6 +144,7 @@ def list_from_loose_letters(loose_dict, index_to_words_dicts):
             except:
                 print("dictionary skipped at letter " + str(letter) + ", index " + str(index))
     return loose_list
+
 
 if __name__ == "__main__":
     index_dicts = [{} for i in range(WORD_LEN)] 
